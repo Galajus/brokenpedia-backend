7a2820409c240665afd666e0465ab753e6eba062
Galajus <galajus@gmail.com>
2023-02-23 10:44:49 +0100

    make app runnable

---
+++ src/main/java/pl/galajus/brokenpediabackend/BrokenpediaBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.galajus.brokenpediabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokenpediaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokenpediaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/galajus/brokenpediabackend/BrokenpediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.galajus.brokenpediabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokenpediaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
