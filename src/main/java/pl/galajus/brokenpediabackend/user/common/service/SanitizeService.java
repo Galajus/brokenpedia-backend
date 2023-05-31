e55a15ee8dd5d02dcf50b996817f736e66ff244f
Galajus <galajus@gmail.com>
2023-06-01 00:57:54 +0200

    null check for sanitizer

--- src/main/java/pl/galajus/brokenpediabackend/user/common/service/SanitizeService.java
+++ src/main/java/pl/galajus/brokenpediabackend/user/common/service/SanitizeService.java
@@ -8,14 +8,23 @@ import org.springframework.stereotype.Service;
 public class SanitizeService {
 
     public static String cleanCompletely(String content) {
+        if (content == null) {
+            return null;
+        }
         return Jsoup.clean(content, Safelist.none());
     }
 
     public static String cleanBasic(String content) {
+        if (content == null) {
+            return null;
+        }
         return Jsoup.clean(content, Safelist.basic());
     }
 
     public static String cleanRelaxed(String content) {
+        if (content == null) {
+            return null;
+        }
         Safelist font = Safelist.relaxed().addTags("font");
         font.addAttributes("font", "size", "color");
         return Jsoup.clean(content, font);
