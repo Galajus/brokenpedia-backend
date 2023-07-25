5dfd90ec794c6da69d2107547683488c4e9b775b
Galajus <galajus@gmail.com>
2023-07-26 01:24:33 +0200

    hix deprecated security methods (csrf, sessionManagement)

--- src/main/java/pl/galajus/brokenpediabackend/user/security/SecurityConfig.java
+++ src/main/java/pl/galajus/brokenpediabackend/user/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,8 +31,8 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/profile/**").authenticated()
                 .anyRequest().permitAll());
 
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilter(
                 new JwtAuthorizationFilter(
                         authenticationManager,
